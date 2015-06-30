546d2310f3c0e84965f27d145d7d208722ef8720
Ye Ma <stevema@Yes-MacBook-Pro.local>
2015-06-30 16:39:47 -0700

    first commit

---
+++ MojioiOSExampleOAuth2/SignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.h
+//  MojioiOSExampleOAuth2
+//
+//  Created by Ye Ma on 2015-06-30.
+//  Copyright (c) 2015 Ye Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
